86e5ae6e156f7e7abb1cfd6e0e4038b63bcfacd1
Setvin <117243445+realsetvin@users.noreply.github.com>
2024-03-25 20:48:46 +0000

    Update gmp.mk

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,7 +1,7 @@
 package=gmp
 $(package)_version=6.2.1
-$(package)_download_path=https://gmplib.org/download/gmp
-$(package)_file_name=gmp-$($(package)_version).tar.bz2
+$(package)_download_path=https://gmplib.org/download/gmp/
+$(package)_file_name=gmp-$($(package)_version).tar.xz
 $(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
 
 define $(package)_set_vars
