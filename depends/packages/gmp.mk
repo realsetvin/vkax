80d3d59b061fb0b61dd42b81eabedbf3d7d333f4
Setvin <117243445+realsetvin@users.noreply.github.com>
2024-03-25 21:47:46 +0000

    Update gmp.mk

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,8 +1,8 @@
 package=gmp
-$(package)_version=6.3.0
-$(package)_download_path=https://gmplib.org/download/gmp/
-$(package)_file_name=gmp-$($(package)_version).tar.gz
-$(package)_sha256_hash=e56fd59d76810932a0555aa15a14b61c16bed66110d3c75cc2ac49ddaa9ab24c
+$(package)_version=6.2.1
+$(package)_download_path=https://gmplib.org/download/gmp
+$(package)_file_name=gmp-$($(package)_version).tar.bz2
+$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
 
 define $(package)_set_vars
 $(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
@@ -21,4 +21,3 @@ endef
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
-
